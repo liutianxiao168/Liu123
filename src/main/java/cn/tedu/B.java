182a2486366d1ddec2ddc4e746a80cc6b3c84729
liutianxiao168 <879755466@qq.com>
2019-11-12 15:33:01 +0800

    Commit

---
+++ src/main/java/cn/tedu/B.java
@@ -0,0 +1,17 @@
+package cn.tedu;
+
+
+
+public class B {
+
+		//封装参数到map集合中
+		
+		public static void main(String[] args) {
+			
+			Emp emp=new Emp();
+			emp.age=100;
+			emp.name="tom";
+			System.out.println(emp);
+		}
+		
+}
